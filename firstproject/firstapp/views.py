91d96e03ef107397d3f71b7edb104cb0f5847fe5
soljun0402 <soljun0402@gmail.com>
2021-05-13 14:31:39 +0900

    first commit

---
+++ firstproject/firstapp/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+def calculate(request):
+  return render(request, "calculate.html")
+
+def result(request):
+  number1 = request.GET['number1']
+  number2 = request.GET['number2']
+  signiture = request.GET['sign']
+
+  for signiture in signiture:
+    if signiture == "+":
+      result = int(number1) + int(number2)
+
+    elif signiture == "-":
+      result = int(number1) - int(number2)
+
+    elif signiture == "*":
+      result = int(number1) * int(number2)
+
+    elif signiture == "/":
+      result = int(number1) / int(number2)
+
+  return render(request, "result.html", {'result': result})
